481f26105915d6c884177a72fa94872ab987d280
Corentinvls <corentin.vallois@gmail.com>
2021-01-19 15:22:21 +0100

    split graphbuilder and TFUtils

--- src/main/java/TFUtils.java
+++ src/main/java/TFUtils.java
@@ -1,13 +1,8 @@
 import org.tensorflow.*;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 public class TFUtils {
@@ -74,56 +69,6 @@ public class TFUtils {
             try (Session s = new Session(g)) {
                 return s.runner().fetch(output.op().name()).run().get(0);
             }
-        }
-    }
-
-    private static class GraphBuilder {
-        private Graph g;
-
-        GraphBuilder(Graph g) {
-            this.g = g;
-        }
-
-        Output div(Output x, Output y) {
-            return binaryOp("Div", x, y);
-        }
-
-        Output sub(Output x, Output y) {
-            return binaryOp("Sub", x, y);
-        }
-
-        Output resizeBilinear(Output images, Output size) {
-            return binaryOp("ResizeBilinear", images, size);
-        }
-
-        Output expandDims(Output input, Output dim) {
-            return binaryOp("ExpandDims", input, dim);
-        }
-
-        Output cast(Output value, DataType dtype) {
-            return g.opBuilder("Cast", "Cast").addInput(value).setAttr("DstT", dtype).build().output(0);
-        }
-
-        Output decodeJpeg(Output contents, long channels) {
-            return g.opBuilder("DecodeJpeg", "DecodeJpeg")
-                    .addInput(contents)
-                    .setAttr("channels", channels)
-                    .build()
-                    .output(0);
-        }
-
-        Output constant(String name, Object value) {
-            try (Tensor t = Tensor.create(value)) {
-                return g.opBuilder("Const", name)
-                        .setAttr("dtype", t.dataType())
-                        .setAttr("value", t)
-                        .build()
-                        .output(0);
-            }
-        }
-
-        private Output binaryOp(String type, Output in1, Output in2) {
-            return g.opBuilder(type, type).addInput(in1).addInput(in2).build().output(0);
         }
     }
 
