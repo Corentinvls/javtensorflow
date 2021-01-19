29ac986628b7631cdf54a521e4455856fb2a4870
Corentinvls <corentin.vallois@gmail.com>
2021-01-19 15:35:43 +0100

    import labels and model done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,5 +12,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        Utils utils =new Utils();
+
+        // path : ../../inception5h/
+        if (args.length != 2) {
+            System.exit(1);
+        }
+        String modelDir = args[0];
+        String imageFile = args[1];
+
+        byte[] graphDef = utils.readAllBytesOrExit(Paths.get(modelDir, "tensorflow_inception_graph.pb"));
+        List<String> labels = utils.readAllLinesOrExit(Paths.get(modelDir, "labels.txt"));
+        byte[] imageBytes = utils.readAllBytesOrExit(Paths.get(imageFile));
     }
 }
---
+++ src/main/java/Utils.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Utils {
+     byte[] readAllBytesOrExit(Path path) {
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
+   List<String> readAllLinesOrExit(Path path) {
+        try {
+            return Files.readAllLines(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
+}
