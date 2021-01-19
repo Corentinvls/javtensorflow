32d0c03d5c14d3fbfcf8fb75c255bf384b8b5112
Corentinvls <corentin.vallois@gmail.com>
2021-01-19 15:46:55 +0100

    story 1 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 
 
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -14,6 +18,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Utils utils =new Utils();
+        TFUtils tfutils = new TFUtils();
 
         // path : ../../inception5h/
         if (args.length != 2) {
@@ -25,5 +30,40 @@ public class Main {
         byte[] graphDef = utils.readAllBytesOrExit(Paths.get(modelDir, "tensorflow_inception_graph.pb"));
         List<String> labels = utils.readAllLinesOrExit(Paths.get(modelDir, "labels.txt"));
         byte[] imageBytes = utils.readAllBytesOrExit(Paths.get(imageFile));
+
+        try (Tensor image = tfutils.byteBufferToTensor(imageBytes)) {
+            float[] labelProbabilities = executeInceptionGraph(graphDef, image);
+            int bestLabelIdx = maxIndex(labelProbabilities);
+            System.out.printf("BEST MATCH: %s (%.2f%% likely)%n",
+                    labels.get(bestLabelIdx),
+                    labelProbabilities[bestLabelIdx] * 100f);
+        }
+    }
+    private static float[] executeInceptionGraph(byte[] graphDef, Tensor<Float> image) {
+        try (Graph g = new Graph()) {
+            g.importGraphDef(graphDef);
+            try (Session s = new Session(g);
+                 Tensor<Float> result =
+                         s.runner().feed("input", image).fetch("output").run().get(0).expect(Float.class)) {
+                final long[] rshape = result.shape();
+                if (result.numDimensions() != 2 || rshape[0] != 1) {
+                    throw new RuntimeException(
+                            String.format(
+                                    "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
+                                    Arrays.toString(rshape)));
+                }
+                int nlabels = (int) rshape[1];
+                return result.copyTo(new float[1][nlabels])[0];
+            }
+        }
+    }
+    private static int maxIndex(float[] probabilities) {
+        int best = 0;
+        for (int i = 1; i < probabilities.length; ++i) {
+            if (probabilities[i] > probabilities[best]) {
+                best = i;
+            }
+        }
+        return best;
     }
 }
