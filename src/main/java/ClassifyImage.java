9e817777ea3f94a2c3e12abc55f30005e9e55d22
Corentinvls <corentin.vallois@gmail.com>
2021-01-20 10:09:09 +0100

    refactor classify image

--- src/main/java/ClassifyImage.java
+++ src/main/java/ClassifyImage.java
@@ -9,38 +9,46 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ClassifyImage {
+    private static Utils utils = new Utils();
+    private static TFUtils tfutils = new TFUtils();
+
     /**
      * Function to display the result of an image classifaction in the terminal
+     *
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-
-        Utils utils =new Utils();
-        TFUtils tfutils = new TFUtils();
-
         // path : ../../inception5h/
         if (args.length != 2) {
             System.exit(1);
         }
         String modelDir = args[0];
         String imageFile = args[1];
+        displayClassify(modelDir, imageFile);
+    }
 
-        byte[] graphDef = utils.readAllBytesOrExit(Paths.get(modelDir, "tensorflow_inception_graph.pb"));
+    private static void displayClassify(String modelDir, String imageFile) {
         List<String> labels = utils.readAllLinesOrExit(Paths.get(modelDir, "labels.txt"));
-        byte[] imageBytes = utils.readAllBytesOrExit(Paths.get(imageFile));
+        float[] labelProbabilities = getClassify(modelDir, imageFile);
+        System.out.println(Arrays.toString(labelProbabilities));
+        int bestLabelIdx = utils.bestMatch(labelProbabilities);
+        System.out.printf("BEST MATCH: %s (%.2f%% likely)%n",
+                labels.get(bestLabelIdx),
+                labelProbabilities[bestLabelIdx] * 100f);
+    }
 
+    private static float[] getClassify(String modelDir, String imageFile) {
+        byte[] graphDef = utils.readAllBytesOrExit(Paths.get(modelDir, "tensorflow_inception_graph.pb"));
+        byte[] imageBytes = utils.readAllBytesOrExit(Paths.get(imageFile));
         try (Tensor image = tfutils.byteBufferToTensor(imageBytes)) {
-            float[] labelProbabilities = executeClassify(graphDef, image);
-            int bestLabelIdx = utils.bestMatch(labelProbabilities);
-            System.out.printf("BEST MATCH: %s (%.2f%% likely)%n",
-                    labels.get(bestLabelIdx),
-                    labelProbabilities[bestLabelIdx] * 100f);
+            return executeClassify(graphDef, image);
         }
     }
 
     /**
      * Function to run the Image Classification
+     *
      * @param graphDef
      * @param image
      * @return
